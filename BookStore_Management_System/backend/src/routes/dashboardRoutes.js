09694d973efb355113bc090c442225023f45649b
Vipparthi Keerthana <vipparthikeerthana18@gmail.com>
2026-09-15 19:15:02 +0530

    Delete BookStore_Management_System directory

--- BookStore_Management_System/backend/src/routes/dashboardRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getStats } = require('../controllers/dashboardController');
-const { authenticate } = require('../middleware/auth');
-
-router.get('/stats', authenticate, getStats);
-
-module.exports = router;
